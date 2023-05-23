aee8d4949388f9d2849808ddae6e6053d377944c
Binyameen <bmghafoor@gmail.com>
2023-05-22 20:47:09 -0400

    Test for left-arrow

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -36,3 +36,30 @@ it("should match snapshot", () => {
   const { asFragment } = render(<Carousel />);
   expect(asFragment()).toMatchSnapshot();
 });
+
+it("works when you click on the left arrow", function () {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  expect(
+    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).not.toBeInTheDocument();
+  expect(
+    queryByAltText("Photo by Pratik Patel on Unsplash")
+  ).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(
+    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).toBeInTheDocument();
+  expect(
+    queryByAltText("Photo by Pratik Patel on Unsplash")
+  ).not.toBeInTheDocument();
+});
